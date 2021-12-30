55e35949c5088f7517e56c66a690500bc3221fac
Ryan <yang.zhao@oceanus-plus.com>
2021-12-30 21:41:32 +0800

    职责链模式

--- ryan-design-pattern/src/main/java/com/ryan/codebase/design/pattern/action/strategy/StrategyFactory.java
+++ ryan-design-pattern/src/main/java/com/ryan/codebase/design/pattern/action/strategy/StrategyFactory.java
@@ -12,21 +12,15 @@ import java.util.Map;
  * @version Id: StrategyFactory, v 0.1 2021/12/30 8:32 PM ryan Exp $
  */
 public class StrategyFactory {
-
     private static final Map<String,Strategy> map = new HashMap<>();
-
     static {
         map.put("A",new ConcreteStrategyA());
         map.put("B",new ConcreteStrategyB());
     }
-
     public static Strategy provideConcreteStrategy(String type) throws IllegalAccessException {
-
         if (StringUtils.isEmpty(type)){
             throw new IllegalAccessException("error");
         }
         return map.get(type);
     }
-
-
 }
--- ryan-design-pattern/src/main/java/com/ryan/codebase/design/pattern/action/strategy/StrategyTest.java
+++ ryan-design-pattern/src/main/java/com/ryan/codebase/design/pattern/action/strategy/StrategyTest.java
@@ -5,16 +5,9 @@ package com.ryan.codebase.design.pattern.action.strategy;
  * @version Id: StrategyTest, v 0.1 2021/12/30 8:25 PM ryan Exp $
  */
 public class StrategyTest {
-
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws IllegalAccessException {
         Strategy strategy =null;
         String type = "A";
-        try {
-            StrategyFactory.provideConcreteStrategy(type).strategyMethod();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
+        StrategyFactory.provideConcreteStrategy(type).strategyMethod();
     }
-
-
 }
